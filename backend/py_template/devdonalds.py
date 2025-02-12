460722944bd6a27a45e3baa0a41657aef826c3fe
unknown <quangminh23062006@gmail.com>
2025-02-12 12:55:41 +1100

    Completed task 1 and 2 of backend/py_template

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -43,7 +43,27 @@ def parse():
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
+	goodRecipeName = ''
+	t = False
+	for cha in recipeName:
+		if len(goodRecipeName) != 0:
+			lastCha = goodRecipeName[-1]
+		else:
+			lastCha = 'anything'
+		if cha == '-' or cha == '_' or cha == ' ':
+			if lastCha != ' ':
+				goodRecipeName += ' '
+		elif cha.isalpha():
+			if t == False:
+				goodRecipeName += cha.upper()
+				t = True
+			elif lastCha == ' ':
+				goodRecipeName += cha.upper()
+			else:
+				goodRecipeName += cha.lower()
+	if len(goodRecipeName) == 0:
+		return None
+	recipeName = goodRecipeName
 	return recipeName
 
 
@@ -51,15 +71,33 @@ def parse_handwriting(recipeName: str) -> Union[str | None]:
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	data = request.get_json()
+	entries_type = data.get('type', '')
+	entries_name = data.get('name', '')
+	if entries_type != 'recipe' and entries_type != 'ingredient':
+		return 'Invalid type', 400
+	if entries_name in cookbook:
+		return 'Entry names already exists', 400
+	if entries_type == 'ingredient':
+		cookTime = data.get('cookTime', '')
+		if cookTime <= 0:
+			return 'Invalid cookTime', 400
+	cookbook[entries_name] = data
+	return 'implemented successfully', 200
 
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
+	recipe_name = request.args.get('name', '')
+	if recipe_name not in cookbook:
+		return 'Recipe not found', 400
+	recipe = cookbook[recipe_name]
+	if recipe['type'] != 'recipe':
+		return 'Invalid type', 400
+	total_cook_time = 0
+	ingredients = {}
 	return 'not implemented', 500
 
 
