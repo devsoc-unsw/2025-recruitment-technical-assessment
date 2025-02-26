4e9fdb1244cf625f7230aed9174bf4917414a23f
Andy <andy.ruan2005@gmail.com>
2025-02-26 21:00:42 +1100

    created skeleton for grid

--- frontend/react-app/src/components/BuildingsList.jsx
+++ frontend/react-app/src/components/BuildingsList.jsx
@@ -3,12 +3,18 @@ import data from "../data.json"
 import "../styles/BuildingsList.css"
 
 function BuildingsList() {
+
+    const buildings = data.map(x => {
+        return (
+            <div className="building" style={{ backgroundImage: `url(${x.building_picture})` }}></div>
+        )
+    })
+
     console.log(data[0].building_picture)
     const agsmImg = data[0].building_picture
     return (
         <div className="buildings-container">
-            <div className="building" style={{ backgroundImage: `url(${agsmImg})` }}></div>
-            <div>hi</div>
+            {buildings}
         </div>
     )
 }
