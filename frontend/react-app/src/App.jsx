e3a071e36534fba3cd03340b5c588e7ce4444333
Andy <andy.ruan2005@gmail.com>
2025-02-26 20:25:30 +1100

    add style to buildings list

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './styles/App.css'
 import Header from './components/Header'
+import BuildingsList from './components/BuildingsList'
 
 function App() {
   return (
     <>
       <Header />
+      <BuildingsList />
     </>
   )
 }
---
+++ frontend/react-app/src/components/BuildingsList.jsx
@@ -0,0 +1,17 @@
+import data from "../data.json"
+
+import "../styles/BuildingsList.css"
+
+
+function BuildingsList() {
+    console.log(data[0].building_picture)
+    const agsmImg = data[0].building_picture
+    return (
+        <div className="buildings-container">
+            <div className="building" style={{ backgroundImage: `url(${agsmImg})` }}></div>
+            <div>hi</div>
+        </div>
+    )
+}
+
+export default BuildingsList
