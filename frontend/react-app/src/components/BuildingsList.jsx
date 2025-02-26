010e2efb5696787e450344e6dd817a6ce7bc2a6f
Andy <andy.ruan2005@gmail.com>
2025-02-26 21:28:05 +1100

    got the foundation of the website down

--- frontend/react-app/src/components/BuildingsList.jsx
+++ frontend/react-app/src/components/BuildingsList.jsx
@@ -6,7 +6,18 @@ function BuildingsList() {
 
     const buildings = data.map(x => {
         return (
-            <div className="building" style={{ backgroundImage: `url(${x.building_picture})` }}></div>
+            <div className="building" style={{ backgroundImage: `url(${x.building_picture})` }}>
+                <div className="rooms-available-container">
+                    <div className="rooms-available">
+                        <p>
+                            {x.rooms_available} rooms available
+                        </p>
+                    </div>
+                </div>
+                <div className="building-name">
+                    {x.name}
+                </div>
+            </div>
         )
     })
 
