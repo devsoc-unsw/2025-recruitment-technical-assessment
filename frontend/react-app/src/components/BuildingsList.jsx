079720fd348db5cd27954b9827032e29ed1caf4b
Andy <andy.ruan2005@gmail.com>
2025-02-26 20:45:15 +1100

    changed path for images in data.json

--- frontend/react-app/src/components/BuildingsList.jsx
+++ frontend/react-app/src/components/BuildingsList.jsx
@@ -2,7 +2,6 @@ import data from "../data.json"
 
 import "../styles/BuildingsList.css"
 
-
 function BuildingsList() {
     console.log(data[0].building_picture)
     const agsmImg = data[0].building_picture
